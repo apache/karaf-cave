20e6ece78e92eb887d6e4e713d355513dea8cb07
Jean-Baptiste Onofre <jbonofre@apache.org>
2011-08-21 17:39:57 +0000

    Start the Cave management support. Start the Cave RepositoryAdmin proxy client.
    git-svn-id: https://svn.apache.org/repos/asf/karaf/sandbox/jbonofre/cave/trunk@1160020 13f79535-47bb-0310-9956-ffa450edef68

--- client/src/main/java/org/apache/karaf/cave/client/RepositoryAdminProxy.java
+++ client/src/main/java/org/apache/karaf/cave/client/RepositoryAdminProxy.java
@@ -16,8 +16,45 @@
  */
 package org.apache.karaf.cave.client;
 
+import org.apache.cxf.jaxrs.client.WebClient;
+import org.apache.felix.bundlerepository.DataModelHelper;
+import org.apache.felix.bundlerepository.RepositoryAdmin;
+import org.apache.felix.bundlerepository.Requirement;
+import org.apache.felix.bundlerepository.Resource;
+import org.apache.felix.bundlerepository.impl.DataModelHelperImpl;
+import org.apache.felix.bundlerepository.impl.RepositoryImpl;
+import org.osgi.framework.InvalidSyntaxException;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 /**
  *  Implementation of the OBR RepositoryAdmin service which proxy to a Karaf Cave server.
  */
-public class RepositoryAdminProxy {
+public class RepositoryAdminProxy implements RepositoryAdmin {
+
+    private final String caveServerUrl;
+
+    private final Map<String, RepositoryImpl> repositories = new HashMap<String, RepositoryImpl>();
+    private final DataModelHelper dataModelHelper = new DataModelHelperImpl();
+
+    public RepositoryAdminProxy(String caveServerUrl) {
+        this.caveServerUrl = caveServerUrl;
+    }
+
+    public Resource[] discoverResources(String filter) throws InvalidSyntaxException {
+        // check if the syntax is valid
+        dataModelHelper.filter(filter);
+        // delegate to the Cave server
+        WebClient client = WebClient.create(caveServerUrl + "/obr/resources");
+
+    }
+
+    public Resource[] discoverResources(Requirement[] requirements) {
+        WebClient client = WebClient.create(caveServerUrl + "/obr/resources");
+    }
+
+
 }
---
+++ server/management/src/main/java/org/apache/karaf/cave/server/management/internal/CaveRepositoryMBeanImpl.java
@@ -0,0 +1,75 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.karaf.cave.server.management.internal;
+
+import org.apache.karaf.cave.server.backend.api.CaveRepository;
+import org.apache.karaf.cave.server.backend.api.CaveRepositoryService;
+import org.apache.karaf.cave.server.management.CaveRepositoryMBean;
+
+import javax.management.NotCompliantMBeanException;
+import javax.management.StandardMBean;
+import javax.management.openmbean.*;
+
+/**
+ * Implementation of the Cave repository MBean.
+ */
+public class CaveRepositoryMBeanImpl extends StandardMBean implements CaveRepositoryMBean {
+
+    private CaveRepositoryService caveRepositoryService;
+
+    public CaveRepositoryMBeanImpl() throws NotCompliantMBeanException {
+        super(CaveRepositoryMBean.class);
+    }
+
+    public CaveRepositoryService getCaveRepositoryService() {
+        return this.caveRepositoryService;
+    }
+
+    public void setCaveRepositoryService(CaveRepositoryService caveRepositoryService) {
+        this.caveRepositoryService = caveRepositoryService;
+    }
+
+    public TabularData getCaveRepositories() throws Exception {
+        CaveRepository[] caveRepositories = caveRepositoryService.getRepositories();
+
+        CompositeType caveRepositoryType = new CompositeType("Repository", "Karaf Cave repository",
+                new String[]{"name", "location"},
+                new String[]{"Name of the Cave repository", "Location of the Cave repository storage"},
+                new OpenType[]{SimpleType.STRING, SimpleType.STRING });
+
+        TabularType tableType = new TabularType("Repositories", "Table of all Karaf Cave repositories", caveRepositoryType,
+                new String[]{ "name" });
+
+        TabularData table = new TabularDataSupport(tableType);
+
+        for (int i = 0; i < caveRepositories.length; i++) {
+            CompositeData data = new CompositeDataSupport(caveRepositoryType,
+                    new String[]{ "name", "location" },
+                    new Object[]{ caveRepositories[i].getName(), caveRepositories[i].getLocation() });
+            table.put(data);
+        }
+
+        return table;
+    }
+
+    public void createRepository(String name) throws Exception {
+        caveRepositoryService.createRepository(name, true);
+    }
+
+    public void destroyRepository(String name) throws Exception {
+        caveRepositoryService.destroy(name);
+    }
+
+}
